65cafa5402512e331651f2a2edf145ba9da0bad1
MJoshuaB <13187637+MJoshuaB@users.noreply.github.com>
2021-07-02 18:43:49 +1200

    standardise variable names

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
-# Curses based terminal shaders
+# charsl: a terminal based shading sandbox
+Functions much like shadertoy
 
 ### Goals:
 - header file with the basic types, functions, and defines for simple GLSL style shaders
 - Implement a shadertoy style interface.
+
+### Examples:
+- twinkle; spots appear and fade over time
+
+# License (GNU GPL v3.0)
--- charsl.h
+++ charsl.h
@@ -1,23 +1,18 @@
-/* --------------------------
- *         Header File
- * --------------------------
- */
+ /***************************\
+ *         Header File       *
+ \***************************/
 
 #ifndef CHARSL_H
 #define CHARSL_H
 
 // TODO:
-// add method to sample characters
-//
-// IN    : float : TIME
-// IN    : int   : FRAMECOUNT
-// IN    : ivec2 : FRAGCOORD
-// IN    : ivec2 : RESOLUTION
-// IN    : vec2  : UV
-// INOUT : char  : CHAR
-//
 // texture(Sampler2D texture, vec2 uv)
 // texelFetch(Sampler2D texture, ivec2 fragCoord)
+//            ^~~~~~~~~~~~~~~~^
+// would there be other textures?
+// do I need a Sampler2D struct?
+//
+// void onKeyPressed(); // called when there are keypresses in the buffer
 
 #include <curses.h>
 
@@ -34,11 +29,12 @@ typedef struct {
 
 
 // Global variables
-int frameCount;          // number of frames since start
-vec2 UV;                 // [0.0..1.0] uv coords of the character
-char CHAR;               // char at position
-ivec2 fragCoord;         // integer offset from the top-left of the window
-ivec2 resolution;        // resolution of the screen
+int   FRAMECOUNT;        // number of frames since start
+float TIME;              // time since start
+char  CHAR;              // char at position
+vec2  UV;                // [0.0..1.0] uv coords of the character
+ivec2 FRAGCOORD;         // integer offset from the top-left of the window
+ivec2 RESOLUTION;        // resolution of the screen in chars
 
 // Method definitions
 void fragment(void);
@@ -48,21 +44,21 @@ float fract(float a);
 char texture(vec2 uv); // returns char at nearest fragCoord
 char texelFetch(ivec2 fragCoord); // returns char at fragCoord
 
-// ---------------------
-// Main function
-// ---------------------
+//=-------------=//
+// Main function //
+//=-------------=//
 int main(void) {
 	// setup curses
 	initscr();
 	cbreak();
 	noecho();
 	curs_set(0); // make cursor invisible
-	timeout(0); // make getch non-blocking
+	timeout(0);  // make getch non-blocking
 
 	clear();
 
-	resolution.x = COLS;
-	resolution.y = LINES;
+	RESOLUTION.x = COLS;
+	RESOLUTION.y = LINES;
 
 	// run loop
 	while (getch() != 'q') { // quit when 'q' key is pressed; maybe ESC?
@@ -70,15 +66,15 @@ int main(void) {
 		for (int y = 0; y < LINES; y++) {
 			for (int x = 0; x < COLS; x++) {
 				CHAR = (char)(mvinch(y, x) & A_CHARTEXT);
-				fragCoord.x = x;
-				fragCoord.y = y;
+				FRAGCOORD.x = x;
+				FRAGCOORD.y = y;
 				UV.x = (float)x/(float)COLS;
 				UV.y = (float)y/(float)LINES;
 				fragment();
 				mvaddch(y, x, CHAR);
 			}
 		}
-		frameCount++;
+		FRAMECOUNT++;
 		refresh();
 		napms(50); // sleep for 50 ms
 	}
@@ -92,6 +88,15 @@ int main(void) {
 float fract(float a) {
 	// return the fractional part of a
 	return a - (int)a;
+}
+
+char texture(vec2 uv) {
+	// return the character nearest the uv point
+	return 'X';
+}
+char texelFetch(ivec2 fragCoord) {
+	// return the character at the fragCoord
+	return 'X';
 }
 
 #endif // ifndef CHARSL_H
