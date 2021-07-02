6ee586c3334c83969590833b927bdd250c8fbb38
MJoshuaB <13187637+MJoshuaB@users.noreply.github.com>
2021-07-02 15:25:07 +1200

    add examples dir

---
+++ examples/twinkle.c
@@ -0,0 +1,24 @@
+/***********************\
+* twinke.c              *
+* A smalll example file *
+* for charsl.h          *
+\***********************/
+
+#include <stdlib.h>
+#include "../charsl.h"
+
+char shades[] = " .-:*+=%#@";
+
+void fragment(void) {
+	char c = CHAR;
+	int i;
+	for (i = 1; i < 10; i++) {
+		if (c == shades[i]) {
+			c = shades[i - 1];
+		}
+	}
+	if (c == ' ' && rand() % 1000 == 0) {
+		c = '@';
+	}
+	CHAR = c;
+}
